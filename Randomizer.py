f56af005f11d4257d8095c497a9d24112640c0c6
Etnad13 <danjiji13@gmail.com>
2020-02-20 14:34:13 -0800

    Create Randomizer.py

---
+++ Randomizer.py
@@ -0,0 +1,18 @@
+import random
+
+def rand(n):
+    try:
+        for x in range(0,n+1):
+            print(random.random())
+        return;
+    except ValueError:
+        quit();
+
+def main():
+    rand(int(input("Enter the number of random values less than 1.00 that you'd like to obtain: ")))
+    return;
+
+try:
+    main()
+except ValueError:
+    quit();
