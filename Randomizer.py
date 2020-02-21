a087b7e03d388ede605dc57762a3aa3a24ded5ac
Etnad13 <danjiji13@gmail.com>
2020-02-20 16:35:15 -0800

    Commiting all filetypes I know currently

--- Randomizer.py
+++ Randomizer.py
@@ -2,13 +2,22 @@ import random
 
 def rand(n):
     try:
-        for x in range(0,n+1):
+        for x in range(n+1):
             print(random.random())
         return
     except ValueError:
         quit()
 
+def randInt(n):
+    try:
+        dice = random.randint(1,n)
+        return dice
+    except ValueError:
+        quit()
+
+
 def main():
+    print(randInt(int(input("Enter the amount of sides for the dice you would like to use:"))))
     rand(int(input("Enter the number of random values less than 1.00 that you'd like to obtain: ")))
     return
 
